1b40eb21236c39b52fca3c92688e6dc53e701c11
RichaThanekar <141063653+RichaThanekar@users.noreply.github.com>
2023-10-28 21:57:55 +0530

    updates

--- prices.js
+++ prices.js
@@ -27,9 +27,23 @@ const apiUrl = 'https://api.coingecko.com/api/v3/coins/markets';
                         <td>${crypto.market_cap_rank}</td>
                         <td>Rs.${crypto.current_price}</td>
                         <td>${crypto.price_change_percentage_24h.toFixed(2)}%</td>
+
+                        <td class="star-icon">
+                            <button class="add-to-cart-button" data-coin-id="${crypto.id}">Add to Cart</button>
+                        </td>
                     `;
                     cryptoTable.appendChild(row);
                 });
+                
+                // Handle the click event for "Add to Cart" buttons
+                document.querySelectorAll('.add-to-cart-button').forEach(button => {
+                    button.addEventListener('click', () => {
+                        const coinId = button.getAttribute('data-coin-id');
+                        // Redirect to the new HTML page with the selected coin
+                        window.location.href = `selected_coin.html?id=${coinId}`;
+                    });
+                });
+                
             } catch (error) {
                 console.error('Error fetching cryptocurrency data:', error);
             }
